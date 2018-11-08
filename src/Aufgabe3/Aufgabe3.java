97c339411c3f9f1751dfffafa6cb3536bc8bf9da
ninapfister <1810653169@stud.fh-kufstein.ac.at>
2018-11-08 11:23:07 +0100

    Aufgabe5

--- src/Aufgabe3/Aufgabe3.java
+++ src/Aufgabe3/Aufgabe3.java
@@ -1,17 +1,15 @@
 package Aufgabe3;
 
-public class Aufgabe3
-{
+public class Aufgabe3 {
 
-    public static void main (String[] args)
-    {
+    public static void main(String[] args) {
         String name = "Nina";
         System.out.println(name);
         int alter = 21;
         System.out.println(alter);
         final String FH_KUFSTEIN_TIROL = "Fachhoschule Kufstein Tirol"; // rechtsklick auf Variablennamen, refactor und rename
         // final bedeutet das die ganze Zeile Final geändert wurde, es benötigt als erstes Wort in der Zeile final und dann muss der Variablenname in Grossbuchstaben geschrieben werden
-
-        
+        final String hallo;
     }
+
 }
