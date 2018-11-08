7e3f1ff874688affd5b4f500fd35a2e7bf5e6618
ninapfister <1810653169@stud.fh-kufstein.ac.at>
2018-11-08 11:11:12 +0100

    Aufgabe4

--- src/Aufgabe3/Aufgabe3.java
+++ src/Aufgabe3/Aufgabe3.java
@@ -9,5 +9,9 @@ public class Aufgabe3
         System.out.println(name);
         int alter = 21;
         System.out.println(alter);
+        final String FH_KUFSTEIN_TIROL = "Fachhoschule Kufstein Tirol"; // rechtsklick auf Variablennamen, refactor und rename
+        // final bedeutet das die ganze Zeile Final geändert wurde, es benötigt als erstes Wort in der Zeile final und dann muss der Variablenname in Grossbuchstaben geschrieben werden
+
+        
     }
 }
