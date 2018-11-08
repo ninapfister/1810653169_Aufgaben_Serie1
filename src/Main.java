5616affa127def3143916d10f4ce6a9a912f9591
ninapfister <1810653169@stud.fh-kufstein.ac.at>
2018-11-08 10:35:20 +0100

    Erklärung

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
-public class Main {
+public class Main // erste Klasse die automatisch beim öffnen eines neuen Projekts vorhanden ist
+{
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Nina");
+    public static void main(String[] args) // ist die Deklarierung, die uns später die Werte wieder gibt
+    {
+        System.out.println("Hello World!"); // zeigt uns was er später ausgeben soll
+        System.out.println("Nina"); // zeigt uns was er später ausgeben soll
     }
 }
 
--- src/Package2/NeueKlasse.java
+++ src/Package2/NeueKlasse.java
@@ -1,10 +1,10 @@
-package Package2;
+package Package2; // wurde von uns erstellt, indem wir bei SRC rechte Maustaste gedrückt haben und New - Java Class ausgewählt haben
 
-public class NeueKlasse
+public class NeueKlasse // der Name der Klasse wurde von uns selbst gewählt
 {
-    public static void main(String[] args)
+    public static void main(String[] args) // wir sollten eine Main-Methode erstellen, da wir einen Text ausgeben mussten
     {
-        System.out.println("FH Kufstein Tirol");
+        System.out.println("FH Kufstein Tirol"); // der Text der ausgegeben werden soll, sout danach tap und Text in Klammern schreiben
     }
 
 
